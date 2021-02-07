f94245568c0b173cb3ea454b6e095c772c32bfa0
Alex <alejandroalfonsoacosta@gmail.com>
2021-02-07 14:24:19 -0500

    initial commit

---
+++ src/main/java/com/thera/TheraWebSolutionsServer/TheraWebSolutionsServerApplication.java
@@ -0,0 +1,13 @@
+package com.thera.TheraWebSolutionsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheraWebSolutionsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheraWebSolutionsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thera/TheraWebSolutionsServer/TheraWebSolutionsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thera.TheraWebSolutionsServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheraWebSolutionsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
