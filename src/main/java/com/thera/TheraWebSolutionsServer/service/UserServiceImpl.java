7d4789977daf5e4bd0b3d73ab93929b5f105b696
Luis Manuel Ramos Cespedes <lramos.intl@outlook.com>
2021-03-07 18:39:31 -0500

    overriding findall with filter to implement query

--- src/main/java/com/thera/TheraWebSolutionsServer/service/UserServiceImpl.java
+++ src/main/java/com/thera/TheraWebSolutionsServer/service/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.thera.TheraWebSolutionsServer.service;
 
 import com.thera.TheraWebSolutionsServer.model.User;
+import com.thera.TheraWebSolutionsServer.model.query.LogicalOperator;
+import com.thera.TheraWebSolutionsServer.model.query.QueryCriteria;
+import com.thera.TheraWebSolutionsServer.model.query.QueryOperator;
+import com.thera.TheraWebSolutionsServer.model.query.QuerySpecification;
 import com.thera.TheraWebSolutionsServer.repository.UserRepository;
 import com.thera.TheraWebSolutionsServer.service.core.ServiceCoreImpl;
 import com.thera.TheraWebSolutionsServer.service.filters.UserFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl extends ServiceCoreImpl<User, Integer, UserFilter> implements UserService {
@@ -17,5 +23,20 @@ public class UserServiceImpl extends ServiceCoreImpl<User, Integer, UserFilter> 
     public UserServiceImpl(UserRepository repository) {
         super(repository);
         this.repository = repository;
+    }
+
+    @Override
+    public List<User> findAll(UserFilter filter) {
+        QuerySpecification<User> querySpecification = new QuerySpecification<>();
+        if (filter != null) {
+            if(filter.getName() != null){
+                querySpecification.AddCriteria(new QueryCriteria("name", filter.getName(), QueryOperator.MATCH));
+            }
+            if(filter.getEmail() != null){
+                querySpecification.AddCriteria(new QueryCriteria("email", filter.getEmail(), QueryOperator.MATCH));
+            }
+        }
+
+        return repository.findAll(querySpecification);
     }
 }
